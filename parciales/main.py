85f222e5737467a217591dc8ae52907dac2d3a2f
aixa9 <yessiaisha_15@hotmail.com>
2016-04-11 05:24:32 -0500

    parcial
    integrante del parcial Yeselka Tyrrell, Alberto Juarez

---
+++ parciales/main.py
@@ -0,0 +1,32 @@
+import kivy
+kivy. require('1.9.0')
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Calculador(BoxLayout):
+	def Borrar(self, operacion):
+			self.num.text = operacion[:-1]
+
+	def elim(self, operacion):
+			self.num.text = operacion[:0]
+
+
+	def Calculos(self, operacion):
+		try:
+			self.num.text = str( eval(operacion))
+			print (operacion)
+			print (self.num.text)
+		except:
+			self.num.text = 'Error de sintaxis'
+
+class CalculadorApp(App):
+	title = "Calculadora"
+	def build(self):
+		return Calculador()
+
+if __name__ in ('__main__', '__android__'):
+	CalculadorApp().run()
+
+
+
